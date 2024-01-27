7d07d7bef499660aed9715f53a24d5c69c5a6d56
sorphil <psoriano01@hotmail.com>
2024-01-26 22:41:23 -0500

    feat/ added class that help manage page size for the various indices

---
+++ WMBA5/WMBA5/Utilities/PageSizeHelper.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace WMBA5.Utilities
+{
+    public static class PageSizeHelper
+    {
+        /// <summary>
+        /// Gets the page size coming from either the Select control
+        /// or the cookie.  Otherwise it sets the default of 5
+        /// </summary>
+        /// <param name="httpContext">The HttpContext from the controller</param>
+        /// <param name="pageSizeID">the pageSizeID value from the Request</param>
+        /// <returns></returns>
+        public static int SetPageSize(HttpContext httpContext, int? pageSizeID, string controllerName)
+        {
+            //
+            int pageSize;
+            if (pageSizeID.HasValue)
+            {
+                //Value selected from DDL so use and save it to Cookie
+                pageSize = pageSizeID.GetValueOrDefault();
+                CookieHelper.CookieSet(httpContext, controllerName + "pageSizeValue", pageSize.ToString(), 30);
+                //Set this value as the new default if a custom page size has not been set for a controller
+                CookieHelper.CookieSet(httpContext, "DefaultpageSizeValue", pageSize.ToString(), 480);
+            }
+            else
+            {
+                //Not selected so see if it is in Cookie
+                pageSize = Convert.ToInt32(httpContext.Request.Cookies["pageSizeValue"]);
+            }
+            return (pageSize == 0) ? 5 : pageSize;//Neither Selected or in Cookie so go with default
+        }
+        /// <summary>
+        /// Creates a SelectList for choices for page size
+        /// </summary>
+        /// <param name="pageSize">Current value for the selected option</param>
+        /// <returns></returns>
+        public static SelectList PageSizeList(int? pageSize)
+        {
+            return new SelectList(new[] { "3", "5", "10", "20", "30", "40", "50", "100", "500" }, pageSize.ToString());
+        }
+    }
+}
