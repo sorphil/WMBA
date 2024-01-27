5276bca9653adb0dd634b0542ce266a6253c9cd6
sorphil <psoriano01@hotmail.com>
2024-01-27 00:18:31 -0500

    feat/ added a many to many relationship between Player and Lineup

--- WMBA5/WMBA5/obj/Debug/net7.0/WMBA5.AssemblyInfo.cs
+++ WMBA5/WMBA5/obj/Debug/net7.0/WMBA5.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WMBA5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20b7c4371212d10d80741b96dbb36588a9fad0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WMBA5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WMBA5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
