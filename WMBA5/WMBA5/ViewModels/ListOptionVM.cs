8791942dcf3793ccf87dcaa4905c93c42ef3b564
sorphil <psoriano01@hotmail.com>
2024-01-28 01:55:44 -0500

    feat/ added viewmodel for listbox items

---
+++ WMBA5/WMBA5/ViewModels/ListOptionVM.cs
@@ -0,0 +1,9 @@
+﻿namespace WMBA5.ViewModels
+{
+    public class ListOptionVM
+    {
+        //Used for a ListBoxes
+        public int ID { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
