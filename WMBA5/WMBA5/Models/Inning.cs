553265efe8df93d729b0d350624b62499ebc1be9
Juan Francisco Jacobo Rodriguez <jjacoborodrigue1@ncstudents.niagaracollege.ca>
2024-02-20 11:15:51 -0500

    Changing inning class following the new data model

--- WMBA5/WMBA5/Models/Inning.cs
+++ WMBA5/WMBA5/Models/Inning.cs
@@ -1,18 +1,22 @@
-﻿namespace WMBA5.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WMBA5.Models
 {
     public class Inning
     {
         public int ID { get; set; }
 
-        public int InningNumber { get; set; }
+        public int Balls { get; set; }
 
-        public int RunsScored { get; set; }
+        public int Hits { get; set; }
+        public int Runs { get; set; }
 
-        public int PlayersOut { get; set; }
-
-        public ICollection<PlayerAtBat> PlayerAtBats { get; set; } = new HashSet<PlayerAtBat>();
-
+        [Display(Name = "Foul Balls")]
+        public int FoulBalls { get; set; }
+        public int Strikes { get; set; }
+        public int Out { get; set; }
         //Foreign Key
+        [Display(Name = "Game")]
         public int GameID { get; set; }
         public Game Game { get; set; }
     }
