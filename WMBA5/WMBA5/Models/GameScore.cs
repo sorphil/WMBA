63e19ba9e50450edc9d92be656ee485e56c4a6e0
Juan Francisco Jacobo Rodriguez <jjacoborodrigue1@ncstudents.niagaracollege.ca>
2024-02-20 11:30:18 -0500

    Adding GameScore and PlayerGameScore models

---
+++ WMBA5/WMBA5/Models/GameScore.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WMBA5.Models
+{
+    public class GameScore
+    {
+        public int ID { get; set; }
+        public int Inning { get; set; }
+        public int Balls { get; set; }
+        public int FoulBalls { get; set; }
+        public int Strikes { get; set; }
+        public int Out { get; set; }
+        public int Runs { get; set; }
+        public int Hits { get; set; }
+        //Foreign Keys
+        [Display(Name ="Game")]
+        public int GameID { get;set; }
+        public Game Game { get; set; }
+        public int PlayerAtBatID { get; set; }
+        public PlayerAtBat PlayerAtBat { get; set; }
+        public ICollection<PlayerGameScore> PlayerGameScores { get; set; } = new HashSet<PlayerGameScore>();
+
+    }
+}
