0eb3ff979af270390d45010086413fccd1e6227c
sorphil <psoriano01@hotmail.com>
2024-02-28 03:09:27 -0500

    added vm to calculate total stats from group by gameid

---
+++ WMBA5/WMBA5/ViewModels/GameStatsVM.cs
@@ -0,0 +1,21 @@
+﻿namespace WMBA5.ViewModels
+{
+    public class GameStatsVM
+    {
+        public int GameID { get; set; }
+        public int PlayerID { get; set; }
+        public int TotalGamesPlayed { get; set; }
+
+        public int TotalPlayerAppearances { get; set; }
+
+        public int TotalHits { get; set; }
+
+        public int TotalRunsScored { get; set; }
+
+        public int TotalStrikeOuts { get; set; }
+
+        public int TotalWalks { get; set; }
+
+        public int TotalRBI { get; set; }
+    }
+}
