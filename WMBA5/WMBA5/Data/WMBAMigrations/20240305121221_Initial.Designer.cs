6d4955584bd19730ef31b5c85a9e49a1fbf12777
Jorge <jf69766@gmail.comgit config --global user.email :jf69766@gmail.com>
2024-03-05 07:13:46 -0500

    PLayers and teams upload working (only CSV

--- WMBA5/WMBA5/Data/WMBAMigrations/20240305121221_Initial.Designer.cs
+++ WMBA5/WMBA5/Data/WMBAMigrations/20240305121221_Initial.Designer.cs
@@ -11,7 +11,7 @@ using WMBA5.Data;
 namespace WMBA5.Data.WMBAMigrations
 {
     [DbContext(typeof(WMBAContext))]
-    [Migration("20240305110116_Initial")]
+    [Migration("20240305121221_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
