2f0fd60545235d8b6952bf253de6cc85edb1e9d0
sorphil <psoriano01@hotmail.com>
2024-03-05 02:07:06 -0500

    added stats for team and player

---
+++ WMBA5/WMBA5/ViewModels/TeamScoreVM.cs
@@ -0,0 +1,9 @@
+﻿namespace WMBA5.ViewModels
+{
+    public class TeamScoreVM
+    {
+        public int GameID { get; set; }
+        public int TeamID { get; set; }
+        public int TotalRuns { get; set; }
+    }
+}
