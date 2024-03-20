83609c34fe42e0218912650dc89b2e961c09b20d
Juan Francisco Jacobo Rodriguez <jjacoborodrigue1@ncstudents.niagaracollege.ca>
2024-03-20 17:26:15 -0400

    Quick change to use verified emails

--- WMBA5/WMBA5/Program.cs
+++ WMBA5/WMBA5/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<WMBAContext>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
