4f4626674414217ff9b03ba76b75396165702c58
Juan Francisco Jacobo Rodriguez <jjacoborodrigue1@ncstudents.niagaracollege.ca>
2024-03-25 17:54:38 -0400

    Adding authorization in coach and Division Controllers

--- WMBA5/WMBA5/Controllers/CoachController.cs
+++ WMBA5/WMBA5/Controllers/CoachController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ using WMBA5.Models;
 
 namespace WMBA5.Controllers
 {
+    [Authorize(Roles = "Admin, Rookie Convenor, Intermediate Convenor, Senior Convenor")]
     public class CoachController : ElephantController
     {
         private readonly WMBAContext _context;
