c83765a65d40ed936196a97ae079875fc3eae4d8
sorphil <psoriano01@hotmail.com>
2024-03-26 03:52:00 -0400

    added runners to games and bases as an enum

---
+++ WMBA5/WMBA5/Models/Runner.cs
@@ -0,0 +1,13 @@
+﻿namespace WMBA5.Models
+{
+    public class Runner
+    {
+        public int ID { get; set; }
+        public int PlayerID { get; set; }
+        public Player Player { get; set; }
+        public int GameID { get; set; }
+        public Game Game { get; set; }
+
+        public Base Base { get; set; }
+    }
+}
